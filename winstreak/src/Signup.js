013326162a0bb487e255473a6e3dba5954e4f1c5
JamalBuiltIt <Jamalparker70@gmail.com>
2024-12-24 01:18:11 -0500

    gh-pages

--- winstreak/src/Signup.js
+++ winstreak/src/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { auth } from "./firebase";
+import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
